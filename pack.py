31ff7dd078e0aef32228587b7841ad8abf0700dc
CubicPill <CubicPill@users.noreply.github.com>
2020-03-07 06:07:11 +0800

    complete

--- pack.py
+++ pack.py
@@ -5,32 +5,32 @@ class DeserializationError(Exception):
     pass
 
 
-class QRPackage:
+class FileSlice:
     """
     packet format:
-    'PS'(header, 2bytes)+total(1byte, 1-255)+index(1byte, 0-254)
-    +optional, if index==0[filename_length(1 byte,1-255)+filename(up to 255 bytes)]
+    'PS'(header, 2bytes)+total(1byte, 1-255)+index(1byte, 0-254)+compressed(1byte,bool)
+    +filename_length(1 byte,1-255)+filename(up to 255 bytes)
     +content_length(2 bytes,1-65535)+content(typically less than 3000 bytes)
 
     """
+    HEADER_FIXED_LEN = 7
 
-    def __init__(self, total, index, content: bytes or str,  filename:str=None):
+    def __init__(self, total, index, compressed, content: bytes or str, filename: str = None):
         self.total = total
         self.index = index
+        self.compressed = compressed
         if type(content) == str:
             content = content.encoode('utf8')
         self.content = content
-
-        self.is_first = index==0
         self.filename = filename
 
     def serialize(self):
         data = b''
         data += HEADER
         data += bytes([self.total, self.index])
-        if self.is_first:
-            data += bytes([len(self.filename)])
-            data += bytes(self.filename, encoding='utf8')
+        data += bytes([int(self.compressed)])
+        data += bytes([len(self.filename)])
+        data += bytes(self.filename, encoding='utf8')
         data += (len(self.content)).to_bytes(2, byteorder='big', signed=False)
         data += self.content
         return data
@@ -40,36 +40,35 @@ class QRPackage:
         if not data.startswith(HEADER):
             raise DeserializationError('Invalid header')
         data = data[2:]
-        total, index= data[:2]
+        total, index = data[:2]
         data = data[2:]
-        filename = ''
-        is_first=index==0
-        if bool(is_first):
-            filename_len = data[0]
-            filename = data[1:filename_len + 1].decode('utf8')
-            data = data[filename_len + 1:]
+        compressed = bool(data[0])
+        data = data[1:]
+        filename_len = data[0]
+        filename = data[1:filename_len + 1].decode('utf8')
+        data = data[filename_len + 1:]
 
-        content_length = int.from_bytes(data[:2],byteorder='big', signed=False)
+        content_length = int.from_bytes(data[:2], byteorder='big', signed=False)
         content = data[2:]
         if len(content) != content_length:
-            raise DeserializationError('Incorrect content length')
-        return QRPackage(total, index, content,  filename)
+            raise DeserializationError('Incorrect content length, expected {}, got {}'.format(content_length,len(content)))
+        return FileSlice(total, index, compressed, content, filename)
 
 
 if __name__ == '__main__':
-    q = QRPackage(10, 0, b'12312312313', '123.txt')
+    q = FileSlice(2, 1, False, b'12312312313', '123.txt')
     d = q.serialize()
     print(d)
-    q = QRPackage.deserialize(d)
+    q = FileSlice.deserialize(d)
     d = q.serialize()
     print(d)
-    q = QRPackage.deserialize(d)
+    q = FileSlice.deserialize(d)
     d = q.serialize()
     print(d)
-    q = QRPackage.deserialize(d)
+    q = FileSlice.deserialize(d)
     d = q.serialize()
     print(d)
-    q = QRPackage.deserialize(d)
+    q = FileSlice.deserialize(d)
     d = q.serialize()
     print(d)
-    q = QRPackage.deserialize(d)
+    q = FileSlice.deserialize(d)
--- paperstorage.py
+++ paperstorage.py
@@ -1,30 +1,153 @@
+import argparse
+import gzip
+import os
+import sys
+from math import ceil
+
+import PIL.Image
+import pdf2image
 from qrcode import QRCode
 from qrcode.constants import *
-import pdfrw
 from qrcode.util import BIT_LIMIT_TABLE
+from zbar import Scanner
 
-import sys
+from pack import FileSlice
 
-import argparse
+MAX_LEVEL = 24
+SIZE_LIMIT_ERR_CORR = {
+    ERROR_CORRECT_L: int(BIT_LIMIT_TABLE[ERROR_CORRECT_L][23] / 8),
+    ERROR_CORRECT_M: int(BIT_LIMIT_TABLE[ERROR_CORRECT_M][23] / 8),
+    ERROR_CORRECT_Q: int(BIT_LIMIT_TABLE[ERROR_CORRECT_Q][23] / 8),
+    ERROR_CORRECT_H: int(BIT_LIMIT_TABLE[ERROR_CORRECT_H][23] / 8)
+}
+PAGE_IMAGE_COORD = [
+    (120, 122), (620, 122),
+    (120, 622), (620, 622),
+    (120, 1122), (620, 1122)
+]
 
 
-def file_to_qr(filename, content):
-    pass
+# {1: 1094, 0: 860, 3: 614, 2: 464}
+
+def file_to_slice(filename, content, error_correction, compress):
+    if type(content) == str:
+        content = content.encode('utf8')
+    if len(filename) > 255:
+        raise ValueError('Filename too long, should be at most 255 bytes')
+    slice_max_len = SIZE_LIMIT_ERR_CORR[error_correction] - (FileSlice.HEADER_FIXED_LEN + len(filename))
+    file_slices = list()
+    i = 0
+    bp = 0
+    total_slices = ceil(len(content) / slice_max_len)
+    # tha max can be recomputed to balance
+    slice_max_len = ceil(len(content) / total_slices)
+    while bp < len(content):
+        file_slices.append(FileSlice(total_slices, i, compress, content[bp:bp + slice_max_len], filename))
+        bp += slice_max_len
+        i += 1
+    return file_slices
+
+
+def slice_to_file(slices):
+    filename = slices[0].filename
+    total = slices[0].total
+    contents = [None] * total
+
+    for s in slices:
+        content = s.content
+        if s.compressed:
+            content = gzip.decompress(content)
+        assert s.filename == filename
+        contents[s.index] = content
+    assert None not in contents
+    return filename, b''.join(contents)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('filename', type=str, help='File you want to convert')
-    parser.add_argument('--error-correction', type=str, choices=['L', 'M', 'Q', 'H'], default='M')
+    parser.add_argument('--store', action='store_true', default=False)
+    parser.add_argument('--restore', action='store_true', default=False)
+    parser.add_argument('--output', type=str, help='Output file location')
+    parser.add_argument('--error-correction', type=str, choices=['L', 'M', 'Q', 'H'], default='M',
+                        help='Error correction level. Can be chosen from "L"(Low), "M"(Medium), '
+                             '"Q"(Quarter) or "H"(High). ')
     parser.add_argument('--compress', action='store_true')
-    parser.add_argument('--output')
-    qr = QRCode(error_correction=ERROR_CORRECT_M, border=1)
+    args = parser.parse_args()
+    filename = args.filename
+    print('Input file:', filename)
+    if len(filename) > 80:
+        print('File name too long! Should be less that 70 characters.')
+        sys.exit(1)
+    output_filename = args.output
 
-    img = qr.make_image()
-    img.save('out.png')
-    print(qr.version)
+    error_correction = {
+        'L': ERROR_CORRECT_L,
+        'M': ERROR_CORRECT_M,
+        'Q': ERROR_CORRECT_Q,
+        'H': ERROR_CORRECT_H
+    }[args.error_correction]
+    if args.store and not args.restore:
+        if not output_filename:
+            output_filename = os.path.basename(filename) + '.pdf'
+        if not output_filename.endswith('.pdf'):
+            output_filename = output_filename + '.pdf'
+        print('Convert file to paper, output to', output_filename)
+        with open(filename, 'rb') as f:
+            content = f.read()
+        if args.compress:
+            content = gzip.compress(content)
+        slices = file_to_slice(filename, content, error_correction, args.compress)
+        page_count = ceil(len(slices) / 6)
+        page_images = list()
+        for i in range(page_count):
+            page_image = PIL.Image.new('L', (1190, 1684), color='white')
+            for j in range(min(6, len(slices) - 6 * i)):
+                qr = QRCode(error_correction=error_correction, border=1)
+                qr.add_data(slices[i * 6 + j].serialize())
+                qr_image = qr.make_image()
+                qr_image = qr_image.resize((440, 440))
+                page_image.paste(qr_image, PAGE_IMAGE_COORD[j])
+            page_images.append(page_image)
 
-    img.show()
+        if len(page_images) == 1:
+            page_images[0].save(output_filename)
+        else:
+            page_images[0].save(output_filename, save_all=True, append_images=page_images[1:])
+    elif args.restore:
+        pages = pdf2image.convert_from_path(filename)
+        restored_slices = list()
+        for p in pages:
+            p = p.convert('L')
+            # scale
+            h, w = p.size
+            if h > 1000 or w > 1000:
+                if h > w:
+                    h = 1000
+                    w = int(w * (h / 1000))
+                else:
+                    w = 1000
+                    h = int(h * (w / 1000))
+                p = p.resize((h, w), PIL.Image.HAMMING)
+
+            scanner = Scanner()
+            results = scanner.scan(p)
+            for r in results:
+                _type, data, quality, position = r
+                slice = FileSlice.deserialize(data)
+                restored_slices.append(slice)
+        filename, content = slice_to_file(restored_slices)
+        if not output_filename:
+            name, suffix = os.path.splitext(filename)
+            output_filename = name + '.restore' + suffix
+        print('Convert paper to file, output to', output_filename)
+        with open(output_filename, 'wb') as f:
+            f.write(content)
+
+    else:
+        print('Error: Please specify --store or --restore')
+        sys.exit(1)
+    print('Done!')
 
 
 if __name__ == '__main__':
